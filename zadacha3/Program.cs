b561b2ac1344c1ef32f1a722b704327e5815798f
Ovsiychuk Andrey <a.ovsiychuk89@mail.ru>
2024-02-21 12:11:13 +0300

    задача3

---
+++ zadacha3/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает на вход целое число из отрезка 
+//[10, 99] и показывает наибольшую цифру числа.
+using System;
+class Program
+{
+static void Main()
+{
+Console.Write("Введите число из отрезка [10, 99]: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int first = number / 10;
+int second = number % 10;
+int max = first > second ? first : second;
+Console.WriteLine(max);
+}
+}
